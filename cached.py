596c0cb0d8fca404b12cc7b93f0ef76e91d0e8a5
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2026-03-30 09:16:26 +0300

    Bugfix super().__call__
    -            cls.instances[key] = super().__call__(*args)
    . . .
    +            instance = cls.instances[key] = super().__call__(*args, **kwargs)

--- cached.py
+++ cached.py
@@ -9,16 +9,17 @@ import weakref
 class Cached(type):
     def __init__(cls, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        # cls.instances = weakref.WeakValueDictionary()
-        cls.instances = {}
+        cls.instances = weakref.WeakValueDictionary()
+        # cls.instances = {}
 
     def __call__(cls, *args, **kwargs):
         key = (args, frozenset(kwargs.items()))
         if key in cls.instances:
-            pass
+            instance = cls.instances[key]
         else:
-            cls.instances[key] = super().__call__(*args)
-        return cls.instances[key]
+            instance = cls.instances[key] = super().__call__(*args, **kwargs)
+        # return cls.instances[key]
+        return instance
 
 
 class Spam(metaclass=Cached):
