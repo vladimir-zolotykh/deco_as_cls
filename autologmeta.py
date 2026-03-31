fd337ec5dbc9bac7e47e553faa9c8c977c8456b6
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2026-03-31 10:29:54 +0300

    Set/initialize logger in __new__

--- autologmeta.py
+++ autologmeta.py
@@ -5,32 +5,41 @@ import os
 import functools
 import logging
 
-logger = logging.getLogger(name=__name__)
-logging.basicConfig(
-    # stream=sys.stderr,
-    filename=f".{os.path.splitext(os.path.basename(__file__))[0]}.log",
-    format="%(asctime)s %(message)s",
-    datefmt="%H:%M:%S",
-    level=logging.DEBUG,
-)
 
+def log_it(clsname):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            logger = logging.getLogger(clsname)
+            logger.info(f"{func.__name__} called {args}, {kwargs}")
+            res = func(*args, **kwargs)
+            logger.info(f"{func.__name__} returns {res}")
+            return res
 
-def log_it(func):
-    @functools.wraps(func)
-    def wrapper(*args, **kwargs):
-        logger.info(f"{func.__name__} called {args}, {kwargs}")
-        res = func(*args, **kwargs)
-        logger.info(f"{func.__name__} returns {res}")
-        return res
+        return wrapper
 
-    return wrapper
+    return decorator
 
 
 class AutoLogMeta(type):
     def __new__(mcls, clsname, bases, clsdict):
+
+        logger = logging.getLogger(clsname)
+        logger.setLevel(logging.DEBUG)
+
+        if not logger.handlers:
+            handler = logging.FileHandler(
+                f".{os.path.splitext(os.path.basename(__file__))[0]}.log", mode="w"
+            )
+            formatter = logging.Formatter(
+                "%(asctime)s %(name)s %(message)s", datefmt="%H:%M:%S"
+            )
+            handler.setFormatter(formatter)
+            logger.addHandler(handler)
+
         for attr, value in clsdict.items():
             if callable(value) and not attr.startswith("__"):
-                clsdict[attr] = log_it(value)
+                clsdict[attr] = log_it(clsname)(value)
         return super().__new__(mcls, clsname, bases, clsdict)
 
 
@@ -40,6 +49,13 @@ class Mathbox(metaclass=AutoLogMeta):
 
     def mul(self, a, b):
         return a * b
+
+
+if __name__ == "__main__":
+    import pytest
+    import sys
+
+    pytest.main(sys.argv)
 
 
 def test_autolog(capsys):
